6d56a35b6d43d0fa64ef51b23193a515f5843b57
Ítalo Garleni Rodríguez <italogar6@hotmail.com>
2017-03-29 21:47:42 +0200

    Classification Algorithms and Variable Selection

--- analyzer.R
+++ analyzer.R
@@ -3,7 +3,7 @@ source("utilsFunctions.R")
 
 ##Read files
 trainData <- readDataTrain()
-#testData <- readDataTest()
+testData <- readDataTest()
 
 
 ##################
@@ -33,9 +33,61 @@ for (i in 1:ncol(trainData))
 ##Save imputed data (for future uses)
 write.csv(trainData, "imputedData.csv", row.names = F)
 
+########################
+## Variable selection ##
+########################
+
+weightchiSquared <- filterSelection(trainData,"chiSquared")
+weightentropInfo <- filterSelection(trainData,"entropInfo")
+weightentropInfoRatio <- filterSelection(trainData,"entropInfoRatio")
+weightentropSymm <- filterSelection(trainData,"entropSymm")
+
+final.weight <- (weightchiSquared + weightentropInfo + weightentropInfoRatio + weightentropSymm)/4
+
+final.weight.ordered <- cbind(final.weight, rownames(final.weight))
+final.weight.ordered <- final.weight.ordered[order(final.weight, decreasing = T),]
+
+final.weight.ordered
+
+###variables selected:
+# TOT_VEHICULOS_IMPLICADOS, ZONA_AGRUPADA, CARRETERA, ZONA, TIPO_VIA, TRAZADO_NO_INTERSEC,
+# RED_CARRETERA, ACERAS
+
+
+
+
+#Reset workspace
+
+
+
 #
 source("utilsFunctions.R")
 
 ##Read imputed data (for future uses)
-trainData <- read.csv("imputedData.csv")
+trainDataImputed <- read.csv("imputedData.csv")
+testData <- readDataTest()
+
+##############################
+## Classification algorithm ##
+
+
+##5 cross fold validation setup
+indexes <- seq(1,nrow(trainDataImputed),by=1)
+trainPartitions <- createFolds(indexes, k = 5,
+                               returnTrain = TRUE)
+testPartitions <- list()
+for(i in 1:5){
+  testPartitions[[i]] <- indexes[-trainPartitions[[i]]]
+}
+
+model <- randomForest::randomForest(TIPO_ACCIDENTE ~ TOT_VEHICULOS_IMPLICADOS * 
+                                      ZONA_AGRUPADA * ZONA * TIPO_VIA, data=trainDataImputed,
+                                    ntree=100)
+kagglePrediction <- predict(model, newdata = testData[, -1])
+
+kagglePrediction <- cbind(1:length(kagglePrediction),kagglePrediction)
+colnames(kagglePrediction) <- c("Id","Prediccion")
+write.csv(kagglePrediction, "kagglePrediction.csv", row.names = F, quote = F)
+
+writeKAGGLEData(kagglePrediction)
 
--- utilsFunctions.R
+++ utilsFunctions.R
@@ -7,6 +7,13 @@ library(robCompositions)
 library(outliers)
 library(mvoutlier)
 library(NoiseFiltersR)
+library(FSelector)
+library(caret)
+library(party)
+library(rpart)
+library(adabag)
+library(randomForest)
+library(kernlab)
 
 #########
 ## I/O ##
@@ -28,7 +35,9 @@ readDataTest <- function()
 #KAGGLE output generator
 writeKAGGLEData <- function(results)
 {
-  write.csv(results, "kagglePrediction.csv", row.names = F)
+  results <- cbind(1:length(results),results)
+  results <- rbind(c("Id","Prediccion"))
+  write.csv(results, "kagglePrediction.csv", row.names = F, quote = F)
 }
 
 
@@ -59,7 +68,7 @@ NAconfrontation <- function(dataset, var1, var2)
 #count NA values
 countNA <- function(dataset)
 {
-  n <- dim(trainData)
+  n <- dim(dataset)
   NAvalues <- c(colnames(dataset))
   NAvalues <- cbind(NAvalues,c(1:n[2]))
   for (i in 1:n[2])
@@ -164,7 +173,7 @@ mvoutlierAnomalies <- function(dataset)
 
 
 
-###############################################
+########################
 ## Variable selection ##
 ########################
 
@@ -172,30 +181,76 @@ mvoutlierAnomalies <- function(dataset)
 ## Approximation filters
 ############
 
-##chi.squared
-
-##Correlation
-
-##Entrop
-
-##oneR
-
-##relief
-
-
+filterSelection <- function(dataset, type)
+{
+  switch(type,
+         ##Chi-squared
+         chiSquared =
+         {
+           weights <- FSelector::chi.squared(TIPO_ACCIDENTE~.,dataset)
+           return(weights)
+         },
+         ##Correlation
+         #only for continuous data
+         correlation =
+         {
+           weights <- FSelector::linear.correlation(TIPO_ACCIDENTE~.,dataset)
+           return(weights)
+         },
+         ##Entropy based - information gain
+         #only for discrete data
+         entropInfo =
+         {
+           weights <- FSelector::information.gain(TIPO_ACCIDENTE~.,dataset)
+           return(weights)
+         },
+         ##Entropy based - ratio information gain
+         #only for discrete data
+         entropInfoRatio =
+         {
+           weights <- FSelector::gain.ratio(TIPO_ACCIDENTE~.,dataset)
+           return(weights)
+         },
+         ##Entropy based - symmetrical uncertainty
+         #only for discrete data
+         entropSymm =
+         {
+           weights <- FSelector::symmetrical.uncertainty(TIPO_ACCIDENTE~.,dataset)
+           return(weights)
+         },
+         ##oneR
+         #only for discrete data
+         oneR =
+         {
+           weights <- FSelector::oneR(TIPO_ACCIDENTE~.,dataset)
+           return(weights)
+         }
+         ##relief needs distance formula
+         #relief =
+         #{
+         #  weights <- FSelector::relief(TIPO_ACCIDENTE~.,dataset)
+         #   return(weights)
+         #}
+  )
+}
 ############
 ## Approximation wrapper
 ############
 
 ##best.first.search
 
+
 ##exhaustive.search
+
 
 ##greedy.search
 
+
 ##hill.climbing.search
 
+
 ##cfs
+
 
 ##consistency
 
@@ -205,6 +260,22 @@ mvoutlierAnomalies <- function(dataset)
 ############
 
 ##random.forest.importance
+# type 1 = Predictive reliability reduction
+# type 2 = mean impure node reduction
+approximationRFI <- function(dataset, type)
+{
+  weights <- FSelector::random.forest.importance(TIPO_ACCIDENTE~.,dataset, type)
+  return(weights)
+}
+
+############
+## caret package
+############
+
+
+############
+## boruta package
+############
 
 
 
@@ -253,16 +324,161 @@ variableCountVisualization <- function(dataset, var, class)
 classVariableVisualization <- function(dataset, var, class)
 {
   ggplot(data = dataset, mapping = aes(x = class, y = var)) +
-  geom_boxplot() +
-  coord_flip()
+    geom_boxplot() +
+    coord_flip()
 }
 
 
 
 #######################################
-## Classification methods with 10CFV ##
+## Classification methods with 5CFV ##
 #######################################
 
+
+######
+## Conditional inference Classification Tree
+######
+classificationTree <- function(dataset, kaggleTest, trainPartitions, testPartitions)
+{
+  errors <- c()
+  hits <- c()
+  #Algorithm
+  n <- ncol(dataset)
+  variableClass <- names(dataset)[n]
+  formulaClass <- as.formula(paste(variableClass,"~TOT_VEHICULOS_IMPLICADOS * ZONA_AGRUPADA * CARRETERA * ZONA * TIPO_VIA",sep=""))
+  for(i in 1:5){
+    #generate model
+    model <- ctree(formulaClass, dataset[trainPartitions[[i]], ])
+    #predict over test fold
+    predictions <- predict(model, newdata = dataset[testPartitions[[i]], -n])
+    #Save statistics
+    hits[i] <- sum(dataset[testPartitions[[i]], n] == predictions)
+    errors[i] <- length(dataset[testPartitions[[i]], n]) - hits[i]
+  }
+  accuracy <- mean(hits/(errors+hits))
+  cat("Accuracy:", accuracy)
+  #Predict on KAGGLE test data
+  model <- ctree(formulaClass, dataset)
+  kagglePrediction <- predict(model, newdata = kaggleTest[, -1])
+}
+
+######
+## Bagging
+######
+#Variables input
+maxdp = 5
+minsplt = 15
+baggingAlgorithm <- function(dataset, kaggleTest, trainPartitions, testPartitions, maxdp, minsplt)
+{
+  errors <- c()
+  hits <- c()
+  #Algorithm
+  for(i in 1:5){
+    #generate model
+    model <- adabag::bagging(TIPO_ACCIDENTE ~ ., 
+                             data = dataset[trainPartitions[[i]], ], 
+                             control=rpart::rpart.control(maxdepth=maxdp, minsplit=minsplt))
+    #predict over test fold
+    predictions <- adabag::predict.bagging(model, newdata = dataset[testPartitions[[i]], -n])
+    #Save statistics
+    hits[i] <- sum(dataset[testPartitions[[i]], n] == predictions$class)
+    errors[i] <- length(dataset[testPartitions[[i]], n]) - hits[i]
+  }
+  accuracy <- mean(hits/(errors+hits))
+  cat("Accuracy:", accuracy)
+  #Predict on KAGGLE test data
+  model <- adabag::bagging(TIPO_ACCIDENTE ~ ., data = dataset, 
+                           control=rpart::rpart.control(maxdepth=maxdp, minsplit=minsplt))
+  baggingPrediction <- adabag::predict.bagging(model, newdata = as.data.frame(kaggleTest[, -1]))
+  kagglePrediction <- baggingPrediction$class
+  return(kagglePrediction)
+}
+
+######
+## Boosting
+######
+maxdp = 2
+finalm = 10
+boostingAlgorithm <- function(dataset, kaggleTest, trainPartitions, testPartitions, maxdp, finalm)
+{
+  errors <- c()
+  hits <- c()
+  #Algorithm
+  for(i in 1:5){
+    #generate model
+    model <- adabag::boosting(TIPO_ACCIDENTE ~ ., 
+                              data = dataset[trainPartitions[[i]], ],
+                              mfinal = finalm, 
+                              control = rpart::rpart.control(maxdepth = maxdp))
+    #predict over test fold
+    predictions <- adabag::predict.boosting(model, newdata = dataset[testPartitions[[i]], -n])
+    #Save statistics
+    hits[i] <- sum(dataset[testPartitions[[i]], n] == predictions$class)
+    errors[i] <- length(dataset[testPartitions[[i]], n]) - hits[i]
+  }
+  accuracy <- mean(hits/(errors+hits))
+  cat("Accuracy:", accuracy)
+  #Predict on KAGGLE test data
+  model <- adabag::boosting(TIPO_ACCIDENTE ~ ., data = dataset, mfinal = finalm, 
+                            control = rpart::rpart.control(maxdepth = maxdp))
+  boostingPrediction <- adabag::predict.boosting(model, newdata = as.data.frame(kaggleTest[, -1]))
+  kagglePrediction <- boostingPrediction$class
+  return(kagglePrediction)
+}
+
+######
+## RandomForest
+######
+ntrees = 100
+randomForestAlgorithm <- function(dataset, kaggleTest, trainPartitions, testPartitions, ntrees)
+{
+  errors <- c()
+  hits <- c()
+  #Algorithm
+  for(i in 1:5){
+    #generate model
+    model <- randomForest::randomForest(TIPO_ACCIDENTE ~ TIPO_ACCIDENTE ~ TOT_VEHICULOS_IMPLICADOS * ZONA_AGRUPADA * ZONA * TIPO_VIA, data=dataset[trainPartitions[[i]], ],
+                                        ntree=ntrees)
+    #predict over test fold
+    predictions <- predict(model, newdata = dataset[testPartitions[[i]], -n])
+    #Save statistics
+    hits[i] <- sum(dataset[testPartitions[[i]], n] == predictions)
+    errors[i] <- length(dataset[testPartitions[[i]], n]) - hits[i]
+  }
+  accuracy <- mean(hits/(errors+hits))
+  cat("Accuracy:", accuracy)
+  #Predict on KAGGLE test data
+  model <- randomForest::randomForest(TIPO_ACCIDENTE ~ TOT_VEHICULOS_IMPLICADOS * ZONA_AGRUPADA * ZONA * TIPO_VIA, data=dataset, ntree=ntrees)
+  kagglePrediction <- predict(model, newdata = kaggleTest[, -1])
+  return(kagglePrediction)
+}
+
+######
+## SVM
+######
+SVMalgorithm <- function(dataset, kaggleTest, trainPartitions, testPartitions)
+{
+  errors <- c()
+  hits <- c()
+  #Algorithm
+  for(i in 1:5){
+    #generate model
+    model <- train(TIPO_ACCIDENTE ~ ., data = dataset[testPartitions[[i]], ], method = "svmRadial",
+                   preProc = c("center", "scale"))
+    #predict over test fold
+    predictions <- predict(model, newdata = dataset[testPartitions[[i]], -n])
+    #Save statistics
+    hits[i] <- sum(dataset[testPartitions[[i]], n] == predictions)
+    errors[i] <- length(dataset[testPartitions[[i]], n]) - hits[i]
+  }
+  accuracy <- mean(hits/(errors+hits))
+  cat("Accuracy:", accuracy)
+  model <- train(TIPO_ACCIDENTE ~ ., 
+                 data = dataset, 
+                 method = "svmRadial", preProc = c("center", "scale"))
+  kagglePrediction <- predict(model, newdata = kaggleTest[, -1])
+  return(kagglePrediction)
+}
 
 
 ###############################
